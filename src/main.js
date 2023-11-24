917dc00c0b4bd7040dc675d22d84e302a6584d81
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-24 15:02:24 +0100

    feat: add file config for Deliveboo front-office

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+
+import "./assets/scss/style.scss";
+
+import { router } from "./router";
+
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+library.add(faPaperPlane);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router);
+app.mount("#app");
