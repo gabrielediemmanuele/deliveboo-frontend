b58d4c0b057b623717bd91e5de6bd485c7d2f074
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-26 13:42:32 +0100

    Feat: Now Rest.card receive all (no img).

--- src/components/restaurants/RestaurantCard.vue
+++ src/components/restaurants/RestaurantCard.vue
@@ -19,17 +19,29 @@ export default {
 </script>
 
 <template>
-  <div class="col mt-5 mb-5">
+  <div class="col-lg-4 col-md-6 col-sm-12 mt-4 mb-2">
     <div class="card" style="width: 18rem">
-      <img src="..." class="card-img-top" alt="..." />
+      <!--* da modificare!  -->
+      <img
+        src="https://nepomuk.it/wp-content/uploads/2022/02/pizzeria-sterzing.jpg"
+        class="card-img-top"
+        alt="..."
+      />
       <div class="card-body">
         <h5 class="card-title">{{ restaurant.name }}</h5>
-        <p class="card-text">Descrizione</p>
+        <p class="card-text">{{ restaurant.description }}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">Indrizzo</li>
-        <li class="list-group-item">Numero</li>
-        <li class="list-group-item">Tipologia</li>
+        <li class="list-group-item">{{ restaurant.address }}</li>
+        <li class="list-group-item">{{ restaurant.phone_number }}</li>
+        <li
+          class="list-group-item type-label d-flex justify-content-center"
+          v-for="(type, index) in restaurant.types"
+          :key="type.id"
+          :class="'label-' + type.label"
+        >
+          {{ type.label }}
+        </li>
       </ul>
       <div class="card-body">
         <router-link
@@ -42,4 +54,34 @@ export default {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.type-label {
+  color: rgb(255, 255, 255);
+  padding: 5px 10px;
+  border-radius: 5px;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  width: 40%;
+  text-align: center;
+  margin: 5px auto;
+}
+
+.label-Italiano {
+  background-color: rgb(85, 136, 85);
+}
+
+.label-Francese {
+  background-color: rgb(101, 101, 169);
+}
+
+.label-Americano {
+  background-color: rgb(231, 193, 123);
+}
+.label-Inglese {
+  background-color: rgb(225, 125, 125);
+}
+.disabled {
+  background-color: gray;
+}
+</style>
