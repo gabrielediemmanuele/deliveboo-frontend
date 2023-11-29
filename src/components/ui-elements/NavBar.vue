88fecbd061c08b64b90719ea369e12d33f3a3520
MirunaMb <mirunamarcela98@gmail.com>
2023-11-29 12:25:25 +0100

    add icon in navbar

--- src/components/ui-elements/NavBar.vue
+++ src/components/ui-elements/NavBar.vue
@@ -17,34 +17,31 @@ export default {
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark p-0">
     <div class="container-fluid bg-dark p-2">
-      <router-link class="navbar-brand text-light" :to="{ name: 'home' }"
-        >DeliveBoo</router-link
-      >
-      <button
-        class="navbar-toggler bg-light"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <router-link class="navbar-brand text-light" :to="{ name: 'home' }">DeliveBoo</router-link>
+      <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link
-              class="nav-link active text-light"
-              aria-current="page"
-              :to="{ name: 'home' }"
-              >Homepage</router-link
-            >
+            <router-link class="nav-link active text-light" aria-current="page"
+              :to="{ name: 'home' }">Homepage</router-link>
+          </li>
+          <!-- Aggiunta dell'icona alla barra di navigazione -->
+          <li class="nav-item cart-icon">
+            <font-awesome-icon icon="fa-solid fa-cart-shopping" @click="openCart" />
           </li>
         </ul>
       </div>
     </div>
+
   </nav>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cart-icon {
+  color: aliceblue;
+}
+</style>
