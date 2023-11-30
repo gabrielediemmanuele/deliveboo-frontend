a9e253a065786178acddffe7fbef64994d8bd7b8
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-30 18:26:10 +0100

    feat: add footer bar & import to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import NavBar from "./components/ui-elements/NavBar.vue";
+import FooterBar from "./components/ui-elements/FooterBar.vue";
 
 export default {
   data() {
@@ -10,6 +11,7 @@ export default {
 
   components: {
     NavBar,
+    FooterBar,
   },
 };
 </script>
@@ -17,6 +19,7 @@ export default {
 <template>
   <NavBar></NavBar>
   <router-view></router-view>
+  <FooterBar></FooterBar>
 </template>
 
 <style lang="scss" scoped></style>
