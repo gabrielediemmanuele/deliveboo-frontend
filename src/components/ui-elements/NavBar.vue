730893065bcebde4d68f3f7b8807c7ecc74e815d
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-12-01 17:09:42 +0100

    fix: navbar

--- src/components/ui-elements/NavBar.vue
+++ src/components/ui-elements/NavBar.vue
@@ -8,17 +8,20 @@ export default {
   },
   computed: {
     cartTotal() {
-      return this.cart.reduce((total, item) => total + item.price * item.qty, 0);
-    }
+      return this.cart.reduce(
+        (total, item) => total + item.price * item.qty,
+        0
+      );
+    },
   },
   methods: {
     getQty(id) {
-      let item = this.cart.find(item => item.id === id);
+      let item = this.cart.find((item) => item.id === id);
       if (item !== undefined) return item.qty;
       else return 0;
     },
     added(item) {
-      let itemInCart = this.cart.find(cartItem => cartItem.id === item.id);
+      let itemInCart = this.cart.find((cartItem) => cartItem.id === item.id);
 
       if (itemInCart !== undefined) {
         itemInCart.qty += 1;
@@ -29,14 +32,14 @@ export default {
       this.saveCart();
       this.cartKey += 1;
       // Aggiungi un console.log per visualizzare un messaggio o i dati
-      console.log('Item added:', item); // Ad esempio, qui mostrerà l'oggetto item aggiunto
+      console.log("Item added:", item); // Ad esempio, qui mostrerà l'oggetto item aggiunto
 
       // Oppure stampa il carrello per verificare se è stato aggiornato correttamente
-      console.log('Updated cart:', this.cart);
+      console.log("Updated cart:", this.cart);
     },
 
     remove(id) {
-      let index = this.cart.findIndex(item => item.id === id);
+      let index = this.cart.findIndex((item) => item.id === id);
       if (index !== -1) {
         let item = this.cart[index];
         item.qty -= 1;
@@ -55,13 +58,13 @@ export default {
       if (cart) {
         this.cart = JSON.parse(cart);
       }
-    }
+    },
   },
   created() {
     this.loadCart();
 
     // Aggiorna il carrello quando viene rilevato un cambiamento nello storage locale
-    window.addEventListener('cart-updated', () => {
+    window.addEventListener("cart-updated", () => {
       this.loadCart();
     });
   },
@@ -72,25 +75,46 @@ export default {
   <nav class="navbar navbar-expand-lg bg-body-tertiary p-0">
     <div class="container-fluid p-3">
       <img class="cloche mx-1" src="/img/cloche-white.svg" alt="" />
-      <router-link class="navbar-brand text-light" :to="{ name: 'home' }">DeliveBoo</router-link>
-      <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
+      <router-link class="navbar-brand text-light" :to="{ name: 'home' }"
+        >DeliveBoo</router-link
+      >
+      <button
+        class="navbar-toggler bg-light"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
           <li class="nav-item">
-            <router-link class="nav-link active text-light mx-2" aria-current="page"
-              :to="{ name: 'home' }">Homepage</router-link>
-            <router-link class="nav-link active text-light" aria-current="page"
-              :to="{ name: 'home' }">Homepage</router-link>
+            <router-link
+              class="nav-link active text-light mx-2"
+              aria-current="page"
+              :to="{ name: 'home' }"
+              >Homepage</router-link
+            >
           </li>
           <li class="nav-item">
-            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-              aria-controls="offcanvasRight">
-              Icona carello
+            <a class="nav-link text-light" href="#">Contattaci</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="#">Lavora con noi</a>
+          </li>
+          <li class="nav-item">
+            <button
+              class="btn"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasRight"
+              aria-controls="offcanvasRight"
+            >
+              <font-awesome-icon icon="fa-solid fa-cart-shopping" size="xl" />
             </button>
           </li>
         </ul>
@@ -98,16 +122,28 @@ export default {
     </div>
   </nav>
 
-
   <!-- OFFCANVAS -->
-  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel"
+  >
     <div class="offcanvas-body">
       <div v-if="cart.length > 0">
         <div v-for="item in cart" :key="item.id + cartKey" class="card">
           <h5 class="card-title">{{ item.name }}</h5>
           <p class="card-text">{{ item.price }}</p>
-          <button type="button" class="btn btn-success" @click="added(item)">+</button>
-          <button class="btn btn-warning" type="button" @click="remove(item.id)">-</button>
+          <button type="button" class="btn btn-success" @click="added(item)">
+            +
+          </button>
+          <button
+            class="btn btn-warning"
+            type="button"
+            @click="remove(item.id)"
+          >
+            -
+          </button>
           <h3 class="mt-2" v-text="getQty(item.id)"></h3>
         </div>
         <h3>Cart Total: ${{ cartTotal }}</h3>
@@ -118,10 +154,6 @@ export default {
     </div>
   </div>
 </template>
-
-
-
-
 
 <style lang="scss" scoped>
 .navbar {
