0db21317768dbc6bf40e5b1dc7654ada35da5457
MirunaMb <mirunamarcela98@gmail.com>
2023-12-01 16:26:33 +0100

    add logic and template for cart

--- src/components/ui-elements/NavBar.vue
+++ src/components/ui-elements/NavBar.vue
@@ -1,58 +1,88 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
     return {
-      /* title: "Hello world", */
+      cart: [],
+      cartKey: 0,
     };
   },
+  computed: {
+    cartTotal() {
+      return this.cart.reduce((total, item) => total + item.price * item.qty, 0);
+    }
+  },
+  methods: {
+    getQty(id) {
+      let item = this.cart.find(item => item.id === id);
+      if (item !== undefined) return item.qty;
+      else return 0;
+    },
+    added(item) {
+      let itemInCart = this.cart.find(cartItem => cartItem.id === item.id);
 
-  // components: {
-  //   MyComponent,
-  // },
+      if (itemInCart !== undefined) {
+        itemInCart.qty += 1;
+      } else {
+        item.qty = 1;
+        this.cart.push({ ...item, qty: 1 });
+      }
+      this.saveCart();
+      this.cartKey += 1;
+      // Aggiungi un console.log per visualizzare un messaggio o i dati
+      console.log('Item added:', item); // Ad esempio, qui mostrerà l'oggetto item aggiunto
+
+      // Oppure stampa il carrello per verificare se è stato aggiornato correttamente
+      console.log('Updated cart:', this.cart);
+    },
+
+    remove(id) {
+      let index = this.cart.findIndex(item => item.id === id);
+      if (index !== -1) {
+        let item = this.cart[index];
+        item.qty -= 1;
+        if (item.qty <= 0) {
+          this.cart.splice(index, 1);
+        }
+        this.saveCart();
+        this.cartKey += 1;
+      }
+    },
+    saveCart() {
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+    },
+    loadCart() {
+      const cart = localStorage.getItem("cart");
+      if (cart) {
+        this.cart = JSON.parse(cart);
+      }
+    }
+  },
+  created() {
+    this.loadCart();
+
+    // Aggiorna il carrello quando viene rilevato un cambiamento nello storage locale
+    window.addEventListener('cart-updated', () => {
+      this.loadCart();
+    });
+  },
 };
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark p-0">
     <div class="container-fluid bg-dark p-2">
-      <router-link class="navbar-brand text-light" :to="{ name: 'home' }"
-        >DeliveBoo</router-link
-      >
-      <button
-        class="navbar-toggler bg-light"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <router-link class="navbar-brand text-light" :to="{ name: 'home' }">DeliveBoo</router-link>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link
-              class="nav-link active text-light"
-              aria-current="page"
-              :to="{ name: 'home' }"
-              >Homepage</router-link
-            >
+            <router-link class="nav-link active text-light" aria-current="page"
+              :to="{ name: 'home' }">Homepage</router-link>
           </li>
-
-          <!-- OFFCANVAS -->
-          <li>
-            <button
-              class="btn btn-primary"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasRight"
-              aria-controls="offcanvasRight"
-            >
-              <font-awesome-icon :icon="['fas', 'cart-shopping']" />
+          <li class="nav-item">
+            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+              aria-controls="offcanvasRight">
+              Icona carello
             </button>
           </li>
         </ul>
@@ -60,23 +90,27 @@ export default {
     </div>
   </nav>
 
-  <div
-    class="offcanvas offcanvas-end"
-    tabindex="-1"
-    id="offcanvasRight"
-    aria-labelledby="offcanvasRightLabel"
-  >
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-      <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="offcanvas"
-        aria-label="Close"
-      ></button>
+
+  <!-- OFFCANVAS -->
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-body">
+      <div v-if="cart.length > 0">
+        <div v-for="item in cart" :key="item.id + cartKey" class="card">
+          <h5 class="card-title">{{ item.name }}</h5>
+          <p class="card-text">{{ item.price }}</p>
+          <button type="button" class="btn btn-success" @click="added(item)">+</button>
+          <button class="btn btn-warning" type="button" @click="remove(item.id)">-</button>
+          <h3 class="mt-2" v-text="getQty(item.id)"></h3>
+        </div>
+        <h3>Cart Total: ${{ cartTotal }}</h3>
+      </div>
+      <div v-else>
+        <h1 class="bg-primary text-center mt-5">Il tuo carrello è vuoto</h1>
+      </div>
     </div>
-    <div class="offcanvas-body">...</div>
   </div>
 </template>
+
+
 
 <style lang="scss" scoped></style>
