9c96615927af0fe2f4dd5e3998ae97c26c1d2276
luigitarallo <tarallo.gigi@gmail.com>
2023-12-04 23:09:07 +0100

    feat: add props,methods, template

--- src/components/pages/PaymentForm.vue
+++ src/components/pages/PaymentForm.vue
@@ -1,21 +1,33 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
-    data() {
-        return {
-            title: "PaymentForm"
-        }
-    }
-
-    // components: {
-    //   MyComponent,
-    // },
+  props: {
+    cart: Array, // Assicurati che il componente riceva il carrello come prop
+    totalItem: Number, // Assicurati che il componente riceva totalItem come prop
+  },
+  methods: {
+    removeFromCart(id) {
+      // Implementa la logica per rimuovere un elemento dal carrello
+      this.$emit("remove", id); // Emetti un evento per notificare il componente padre
+    },
+    addToCart(item) {
+      this.$emit("add", item);
+    },
+  },
 };
 </script>
-
 <template>
-    <h1>{{ title }}</h1>
+  <div>
+    <!-- Contenuto del carrello -->
+
+    <div v-for="item in cart" :key="item.id">
+      {{ item.name }} - {{ item.price }} - Qty: {{ item.qty }}
+      <button @click="removeFromCart(item.id)">Remove</button>
+      <button @click="addToCart(item)">Aggiungi</button>
+    </div>
+
+    <!-- Totale carrello -->
+    <div>Cart Total: €{{ totalItem }}</div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
