de9bb640786f37f97643f0ec77339346063586a5
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-06 22:32:51 +0100

    fix comment

--- src/components/pages/RestaurantPage.vue
+++ src/components/pages/RestaurantPage.vue
@@ -236,12 +236,6 @@ export default {
           </div>
           <!-- Altre informazioni sui piatti... -->
         </div>
-        <!-- <div v-if="!totalItem == 0">
-          <h3>Cart Total: €{{ totalItem }}</h3>
-        </div>
-        <h1 class="bg-primary text-center mt-5" v-else>
-          Il tuo carrello è vuoto
-        </h1> -->
       </div>
     </div>
   </div>
