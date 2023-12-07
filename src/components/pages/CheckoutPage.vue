1e4ee6e2ef6654f071ffe2cf551a4fe3d3f10286
MirunaMb <mirunamarcela98@gmail.com>
2023-12-07 18:56:07 +0100

    add template

--- src/components/pages/CheckoutPage.vue
+++ src/components/pages/CheckoutPage.vue
@@ -178,128 +178,64 @@ export default {
 </script>
 
 <template>
-  <!--New Form-->
-  <div class="card px-4">
-    <p class="h8 py-3">Dettagli Pagamento</p>
-    <form class="text-center ">
-      <div class="row gx-3">
-        <div class="col-12">
-          <div class="d-flex  my-3 flex-column">
-            <label for="guest_name" class="text mb-1">Nome <span class="need">*</span></label>
-            <input class="form-control mb-3" v-model="guestName" type="text" name="guest_name" id="guest_name" required
-              minlength="3" maxlength="20">
-            <span style="color: red;" class="error-message" v-if="error.guestName">{{ error.guestName
-            }}</span>
+  <NavBar :isHomePage="true" />
+  <div class="container">
+    <h1>{{ title }}</h1>
+
+    <form @submit.prevent="submitForm">
+      <label for="name" class="form-label">Nome</label>
+      <input v-model="formData.guest_name" type="text" name="name" id="name" class="form-control" />
+
+      <label for="surname" class="form-label">Cognome</label>
+      <input v-model="formData.guest_surname" type="text" name="surname" id="surname" class="form-control" />
+
+      <label for="address" class="form-label">Indirizzo</label>
+      <input v-model="formData.guest_address" type="text" name="address" id="address" class="form-control" />
+
+      <label for="phone" class="form-label">Numero</label>
+      <input v-model="formData.guest_phone" type="text" name="phone" id="phone" class="form-control" />
+
+      <label for="email" class="form-label">Email</label>
+      <input v-model="formData.guest_mail" type="email" name="email" id="email" class="form-control" />
+
+      <!-- Template Carta nuovo  -->
+      <div>
+        <div class="form-group">
+          <label class="text">
+            Numero di carta di credito <span class="need">*</span>
+          </label>
+          <div id="creditCardNumber" class="form-control"></div>
+          <span style="color: red" class="error-message" v-if="error.guestName">Inserire numeri carta</span>
+        </div>
+        <div class="form-group">
+          <div class="riga d-flex">
+            <div class="col-6">
+              <label class="text">
+                Data di scadenza <span class="need">*</span></label>
+              <div id="expireDate" class="form-control"></div>
+              <span style="color: red" class="error-message" v-if="error.guestName">Inserire data di scadenza</span>
+            </div>
+            <div class="col-6">
+              <label class="text"> CVV <span class="need">*</span></label>
+              <div id="cvv" class="form-control"></div>
+              <span style="color: red" class="error-message" v-if="error.guestName">Inserire CVV</span>
+            </div>
           </div>
         </div>
-        <div class="col-12">
-          <div class="d-flex  my-3 flex-column">
-            <label for="guest_lastname" class="text mb-1">Cognome <span class="need">*</span></label>
-            <input class="form-control mb-3" v-model="guestLastname" type="text" name="guest_lastname" id="guest_lastname"
-              required minlength="3" maxlength="20">
-            <span style="color: red;" class="error-message" v-if="error.guestLastname">{{
-              error.guestLastname }}</span>
-          </div>
+      </div>
+
+      <div class="col-12 my-3">
+        <div v-if="this.totalItem <= 0" class="bg-danger mb-3">
+          <span class="ps-3">Carrello vuoto, effettua un ordine.</span>
+          <span class="fas fa-arrow-right"></span>
         </div>
-        <div class="col-12">
-          <div class="d-flex  my-3 flex-column">
-            <label for="guest_address" class="text mb-1">Indirizzo <span class="need">*</span></label>
-            <input class="form-control mb-3" v-model="guestAddress" type="text" name="guest_address" id="guest_address"
-              required minlength="3" maxlength="20">
-            <span style="color: red;" class="error-message" v-if="error.guestAddress">{{ error.guestAddress
-            }}</span>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="d-flex  my-3 flex-column">
-            <label for="guest_mail" class="text mb-1">E-Mail <span class="need">*</span></label>
-            <input class="form-control mb-3" type="email" v-model="guestMail" name="guest_mail" id="guest_mail" required
-              minlength="3" maxlength="30">
-            <span style="color: red;" class="error-message" v-if="error.guestMail">{{ error.guestMail
-            }}</span>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="d-flex my-3  flex-column">
-            <label class="text mb-1" for="guest_phone">Telefono : <span class="need">*</span></label>
-            <input v-model="guestPhone" class="form-control no-number-spinners" type="text" name="guest_phone"
-              id="guest_phone" required minlength="3" maxlength="20">
-            <span style="color: red;" class="error-message" v-if="error.guestPhone">{{ error.guestPhone
-            }}</span>
-          </div>
-        </div>
-        <h2 class="h8 my-3">Dati Bancari</h2>
-        <div>
-          <div class="form-group">
-            <label class="text"> Numero di carta di credito <span class="need">*</span> </label>
-            <div id="creditCardNumber" class="form-control"> </div>
-            <span style="color: red;" class="error-message" v-if="error.guestName">Inserire numeri
-              carta</span>
+        <button type="submit" v-else class="btn btn-warning mb-3">
+          <span class="ps-3">Paga €{{ this.totalItem }}</span>
+          <div v-show="payLoad" class="cell"></div>
+        </button>
+      </div>
 
-          </div>
-          <div class="form-group">
-            <div class="riga d-flex">
-              <div class="col-6">
-                <label class="text"> Data di scadenza <span class="need">*</span></label>
-                <div id="expireDate" class="form-control"> </div>
-                <span style="color: red;" class="error-message" v-if="error.guestName">Inserire data di
-                  scadenza</span>
-
-              </div>
-              <div class="col-6">
-                <label class="text"> CVV <span class="need">*</span></label>
-                <div id="cvv" class="form-control"> </div>
-                <span style="color: red;" class="error-message" v-if="error.guestName">Inserire
-                  CVV</span>
-
-                <label for="address" class="form-label">Indirizzo</label>
-                <input v-model="formData.guest_address" type="text" name="address" id="address" class="form-control" />
-
-                <label for="phone" class="form-label">Numero</label>
-                <input v-model="formData.guest_phone" type="text" name="phone" id="phone" class="form-control" />
-
-                <label for="email" class="form-label">Email</label>
-                <input v-model="formData.guest_mail" type="email" name="email" id="email" class="form-control" />
-
-                <!-- Template Carta nuovo  -->
-                <div>
-                  <div class="form-group">
-                    <label class="text">
-                      Numero di carta di credito <span class="need">*</span>
-                    </label>
-                    <div id="creditCardNumber" class="form-control"></div>
-                    <span style="color: red" class="error-message" v-if="error.guestName">Inserire numeri carta</span>
-                  </div>
-                  <div class="form-group">
-                    <div class="riga d-flex">
-                      <div class="col-6">
-                        <label class="text">
-                          Data di scadenza <span class="need">*</span></label>
-                        <div id="expireDate" class="form-control"></div>
-                        <span style="color: red" class="error-message" v-if="error.guestName">Inserire data di
-                          scadenza</span>
-                      </div>
-                      <div class="col-6">
-                        <label class="text"> CVV <span class="need">*</span></label>
-                        <div id="cvv" class="form-control"></div>
-                        <span style="color: red" class="error-message" v-if="error.guestName">Inserire CVV</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-12 my-3">
-                  <div v-if="this.totalItem <= 0" class="bg-danger mb-3">
-                    <span class="ps-3">Carrello vuoto, effettua un ordine.</span>
-                    <span class="fas fa-arrow-right"></span>
-                  </div>
-                  <button type="submit" v-else class="btn btn-warning mb-3">
-                    <span class="ps-3">Paga €{{ this.totalItem }}</span>
-                    <div v-show="payLoad" class="cell"></div>
-                  </button>
-                </div>
-
-                <!-- <h3>Totale da pagare: € {{ this.totalItem }}</h3>
+      <!-- <h3>Totale da pagare: € {{ this.totalItem }}</h3>
 
       <button type="submit" class="btn btn-primary mt-4">Invia dati</button> -->
     </form>
